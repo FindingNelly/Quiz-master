26a4de682669a8d45d105c1a481a3923e58168ac
Adrian Bjørdal <Findingnelly@proton.me>
2022-07-11 16:05:13 +0200

    Arrays

--- Assets/Scrips/QuestionSO.cs
+++ Assets/Scrips/QuestionSO.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(menuName ="Question", fileName ="Question")]
+
+
+
+[CreateAssetMenu(menuName = "Question", fileName = "Question")]
 public class QuestionSO : ScriptableObject
 {
-    [TextArea(2,6)]
-    [SerializeField] string EnterQuestionHere=""; 
+    [TextArea(2, 6)]
+    [SerializeField] string EnterQuestionHere = "";
+
+    public string GetQuestion()
+    {
+        return EnterQuestionHere;
+    }
+    
+    
 }
