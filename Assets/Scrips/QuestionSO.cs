b331384c231deba4155fbedbe5f55482f7c00f98
Adrian Bjørdal <Findingnelly@proton.me>
2022-07-11 18:34:02 +0200

    For Loops

--- Assets/Scrips/QuestionSO.cs
+++ Assets/Scrips/QuestionSO.cs
@@ -9,11 +9,22 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string EnterQuestionHere = "";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswer;
 
     public string GetQuestion()
     {
         return EnterQuestionHere;
     }
-    
-    
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswer()
+    {
+        return correctAnswer;
+    }
+
 }
